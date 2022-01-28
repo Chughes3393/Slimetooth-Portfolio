b8294122281840a13266dc793079598bebc39f43
chughes3393 <hughes.c33@gmail.com>
2022-01-28 12:03:16 -0500

    first commit, API works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+function getAdvice() {
+
+  const resDiv = document.querySelector('#results');
+  const resBtn = document.querySelector('#getData');
+
+  fetch('	https://api.adviceslip.com/advice')
+    .then(response => { return response.json(); })
+    .then(adviceData => {
+      const Adviceobj = adviceData.slip;
+      resDiv.innerHTML = `<p>${Adviceobj.advice}</p>`;
+    }).catch(error => { console.log(error); });
+
+  resBtn.addEventListener('click', () => {
+    getAdvice();
+  });
+
+  window.onload = () => {
+    getAdvice();
+  };
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div id='results'></div>
+      <button onClick={getAdvice} id='getData'>Get Advice</button>
+
     </div>
   );
 }
